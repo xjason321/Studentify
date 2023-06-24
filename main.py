3876e6a59d24acaa3a8896521c2eee88be3fca5e
xjason321 <80063717+xjason321@users.noreply.github.com>
2023-06-24 10:20:36 -0700

    eek

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ Coders: Jason Xie, Subhash Srinivasa, Komal Tummala
 """
 
 from flask import Flask, render_template, request
-import tensorflow as tf
-import numpy as np
-import json
 
 app = Flask(__name__, static_folder='static')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
