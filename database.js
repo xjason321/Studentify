02e0ef844b5d3334f91e09552a542494d9bcf789
SubhashSrinivasa <apssrini09@gmail.com>
2023-06-24 19:43:05 -0700

    blah

--- database.js
+++ database.js
@@ -2,6 +2,7 @@ var input1 = document.getElementById("new-username");
 var input2 = document.getElementById("new-password");
 var signup = document.getElementById("signup")
 var jsonText = document.getElementById("jsontext")
+
 signup.addEventListener("click", function(){
     var data = {
         "username":input1.value,
