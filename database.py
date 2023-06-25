662bf766aab1b661a91a3b4b33896a60da1b7579
AuthXD <122140751+AuthXD@users.noreply.github.com>
2023-06-25 02:07:00 +0000

    idk but test this after jason comes

---
+++ database.py
@@ -0,0 +1,40 @@
+import json
+
+def create_database(username, password):
+    try:
+        # Read the existing database file
+        with open('database.json', 'r') as f:
+            database = json.load(f)
+    except FileNotFoundError:
+        # If the file doesn't exist, create an empty database
+        database = {}
+        database['users'] = []
+
+    # Append the new user to the 'users' list
+    database['users'].append({
+        'username': username,
+        'password': password
+    })
+
+    # Write the updated database back to the file
+    with open('database.json', 'w') as f:
+        json.dump(database, f)
+
+def main():
+    username = input('Enter username: ')
+    password = input('Enter password: ')
+
+    create_database(username, password)
+
+if __name__ == '__main__':
+    main()
+
+
+def extract_input(html_file, id):
+    response = requests.get(html_file)
+    soup = bs4.BeautifulSoup(response.content, 'html.parser')
+
+    input_tag = soup.find(id=id)
+    input_value = input_tag['value']
+
+    return input_value
